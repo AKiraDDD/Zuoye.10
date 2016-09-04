80dfb6ad0393a0581a16f096da764424bc260978
AKiraDDD <923771143@qq.com>
2016-09-04 15:25:30 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/zuoye10/AnotherActivity.java
@@ -0,0 +1,25 @@
+package cn.edu.bistu.cs.se.zuoye10;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("String");
+        Integer age=intent.getIntExtra("int", 20);
+        Toast.makeText(this,name+age,Toast.LENGTH_LONG).show();
+
+    }
+}
